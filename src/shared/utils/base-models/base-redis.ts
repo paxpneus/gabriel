1828e0470a20694404a31a9b30ff40ed737dceaf
gabriel-estoquelogia <gabriel@estoquelogia.com>
2026-04-06 22:51:00 -0300

    FEAT: Added cache to query n + 1

--- src/shared/utils/base-models/base-redis.ts
+++ src/shared/utils/base-models/base-redis.ts
@@ -1,45 +1,74 @@
 import { redisClient } from "../../../config/redis";
 import Redis from "ioredis";
 
+type RedisExpireMode = "EX" | "PX" | "EXAT" | "PXAT" | "KEEPTTL";
+type RedisCondition = "NX" | "XX";
+type RedisSpecialMethods = "GET";
 
 class RedisService {
-    public client: Redis
+  public client: Redis;
 
-    constructor(client: Redis) {
-        this.client = client
+  constructor(client: Redis) {
+    this.client = client;
+  }
+
+  async get<T>(key: string): Promise<T | null> {
+    const data = await this.client.get(JSON.stringify(key));
+
+    if (data == null) return null;
+
+    try {
+      return JSON.parse(data) as T;
+    } catch {
+      return data as unknown as T;
+    }
+  }
+
+  async set<T>(
+    key: string,
+    value: unknown,
+    options?: {
+      mode?: RedisExpireMode;
+      duration?: number;
+      condition?: RedisCondition;
+      specialMethod?: RedisSpecialMethods;
+    },
+  ): Promise<T | null> {
+    const stringValue = JSON.stringify(value);
+
+    if (!options?.mode) {
+      const result = await this.client.set(key, stringValue);
+      return this.handleResponse<T>(result);
     }
 
+    const data = await this.client.set(
+      key,
+      stringValue,
+      options.mode as any,
+      options.duration || 0,
+      (options.condition || "") as any,
+      (options.specialMethod || "") as any,
+    );
 
-    async get<T>(key: string): Promise<T | null> {
-        const data = await this.client.get(JSON.stringify(key));
+    return this.handleResponse<T>(data);
+  }
 
-        if (data == null) return null
-
-        try {
-            return JSON.parse(data) as T
-        } catch {
-            return data as unknown as T
-        } 
-    }
-
-      async set<T>(key: string, value: unknown): Promise<T | null> {
-        const data = await this.client.set(key, JSON.stringify(value));
-        
-        if (data == null) return null
-
-        try {
-            return JSON.parse(data) as T
-        } catch {
-            return data as unknown as T
-        } 
-    }
-
-    async delete(key: string): Promise<void> {
+  async delete(key: string): Promise<void> {
     await this.client.del(key);
+  }
+
+  private handleResponse<T>(data: string | null): T | null {
+  if (data === null) return null;
+  if (data === "OK") return data as unknown as T;
+
+  try {
+    return JSON.parse(data) as T;
+  } catch {
+    return data as unknown as T;
+  }
+}
 }
 
-}
-
-export const redisService = new RedisService(redisClient)
-export const redisConnection = redisClient
+export const redisService = new RedisService(redisClient);
+export const redisConnection = redisClient;
 export default redisService;
