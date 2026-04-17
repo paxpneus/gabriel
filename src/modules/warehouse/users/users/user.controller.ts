69d0cca0a085ab877635107ed82b7fb92866073c
gabriel-estoquelogia <gabriel@estoquelogia.com>
2026-04-16 22:15:46 -0300

    FIX: Removed middleware while front does not have domain

--- src/modules/warehouse/users/users/user.controller.ts
+++ src/modules/warehouse/users/users/user.controller.ts
@@ -25,17 +25,32 @@ export class UserController extends BaseController<User, typeof UserService> {
     this.router.post(`/logout`, this.logout);
   }
 
-  protected middlewaresFor() {
+  // protected middlewaresFor() {
+  //   return {
+  //     index: [authenticate],
+  //     create: [validateCreate(CreateUserSchema)],
+  //     update: [
+  //       authenticate,
+  //       validateId(UserIdSchema),
+  //       validateUpdate(UpdateUserSchema),
+  //     ],
+  //     show: [authenticate, validateId(UserIdSchema)],
+  //     destroy: [authenticate, validateId(UserIdSchema)],
+  //     login: [validateLoginSchema(LoginSchema)],
+  //   };
+  // }
+
+   protected middlewaresFor() {
     return {
-      index: [authenticate],
+      index: [],
       create: [validateCreate(CreateUserSchema)],
       update: [
-        authenticate,
+        
         validateId(UserIdSchema),
         validateUpdate(UpdateUserSchema),
       ],
-      show: [authenticate, validateId(UserIdSchema)],
-      destroy: [authenticate, validateId(UserIdSchema)],
+      show: [validateId(UserIdSchema)],
+      destroy: [validateId(UserIdSchema)],
       login: [validateLoginSchema(LoginSchema)],
     };
   }
