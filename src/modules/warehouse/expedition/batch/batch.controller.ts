d7847339dbb6b2a32907f8ee91133c3f6726ee35
gabriel-estoquelogia <gabriel@estoquelogia.com>
2026-04-17 10:29:10 -0300

    FIX: Changed time to verify to 30 minutes

--- src/modules/warehouse/expedition/batch/batch.controller.ts
+++ src/modules/warehouse/expedition/batch/batch.controller.ts
@@ -1,9 +1,12 @@
-import { Request, Response } from 'express';
-import BaseController from '../../../../shared/utils/base-models/base-controller';
-import ExpeditionBatch from './batch.model';
-import ExpeditionBatchService from './batch.service';
+import { Request, Response } from "express";
+import BaseController from "../../../../shared/utils/base-models/base-controller";
+import ExpeditionBatch from "./batch.model";
+import ExpeditionBatchService from "./batch.service";
 
-export class ExpeditionBatchController extends BaseController<ExpeditionBatch, typeof ExpeditionBatchService> {
+export class ExpeditionBatchController extends BaseController<
+  ExpeditionBatch,
+  typeof ExpeditionBatchService
+> {
   constructor() {
     super(ExpeditionBatchService);
     this.registerCustomRoutes();
@@ -11,54 +14,81 @@ export class ExpeditionBatchController extends BaseController<ExpeditionBatch, t
 
   private registerCustomRoutes(): void {
     // POST /expedition-batches/generate-from-invoices
-    this.router.post('/generate-from-invoices', (req, res) =>
-      this.generateBatchesFromInvoices(req, res)
+    this.router.post("/generate-from-invoices", (req, res) =>
+      this.generateBatchesFromInvoices(req, res),
     );
 
-    this.router.get('/by-invoices/get', this.getBatchesByInvoice)
+    this.router.get("/by-invoices/get", this.getBatchesByInvoice);
+
+    this.router.get("/full/get", this.getFullBatch)
   }
 
   /**
    * POST /expedition-batches/generate-from-invoices
    * Body: { invoiceIds: string[] }
    */
-  generateBatchesFromInvoices = async (req: Request, res: Response): Promise<Response> => {
+  generateBatchesFromInvoices = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
     try {
-      const { invoiceIds } = req.body;
+      const { invoiceIds } = req.params;
 
       if (!Array.isArray(invoiceIds) || invoiceIds.length === 0) {
-        return res.status(400).json({ error: 'Informe ao menos uma nota fiscal (invoiceIds).' });
+        return res
+          .status(400)
+          .json({ error: "Informe ao menos uma nota fiscal (invoiceIds)." });
       }
 
-      const batches = await ExpeditionBatchService.generateBatchFromInvoices(invoiceIds);
+      const batches =
+        await ExpeditionBatchService.generateBatchFromInvoices(invoiceIds);
       return res.status(201).json(batches);
     } catch (error: any) {
       return res.status(500).json({ error: error.message });
     }
   };
 
-  getBatchesByInvoice = async (req: Request, res: Response): Promise<Response> => {
+  getBatchesByInvoice = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
     try {
-    let ids: string[] = [];
- 
-    if (Array.isArray(req.query.invoiceIds)) {
-      ids = req.query.invoiceIds as string[];
-    } else if (typeof req.query.invoiceIds === 'string') {
-      ids = req.query.invoiceIds.split(',').map((s) => s.trim()).filter(Boolean);
+      let ids: string[] = [];
+
+      if (Array.isArray(req.query.invoiceIds)) {
+        ids = req.query.invoiceIds as string[];
+      } else if (typeof req.query.invoiceIds === "string") {
+        ids = req.query.invoiceIds
+          .split(",")
+          .map((s) => s.trim())
+          .filter(Boolean);
+      }
+
+      if (!ids.length) {
+        return res.status(400).json({ error: "Nenhum invoiceId informado." });
+      }
+
+      const batches = await this.service.getBatchesByInvoiceIds(ids);
+      return res.json(batches);
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message });
     }
- 
-    if (!ids.length) {
-      return res.status(400).json({ error: 'Nenhum invoiceId informado.' });
-    }
- 
-    const batches = await this.service.getBatchesByInvoiceIds(ids);
-    return res.json(batches);
-  } catch (err: any) {
-    return res.status(400).json({ error: err.message });
-  }
   };
 
-  
+  getFullBatch = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { batchId, number } = req.query
+
+      console.log(batchId, number)
+
+      const fullBatch = await this.service.findByIdFullBatch(batchId as string ?? '', number as string ?? '')
+
+      return res.json(fullBatch);
+
+    } catch (error: any) {
+      return res.status(400).json({ error: error.message });
+    }
+  };
 }
 
 export default new ExpeditionBatchController();
--- src/modules/warehouse/expedition/batch/batch.types.ts
+++ src/modules/warehouse/expedition/batch/batch.types.ts
@@ -1,3 +1,6 @@
+import { InvoiceItemsAttributes } from '../../entrance/invoice-items/invoice-items.types';
+import { InvoiceAttributes } from '../../entrance/invoice/invoice.types';
+import { ExpeditionBatchInvoiceAttributes } from './../batch-invoices/batch-invoices.types';
 export interface ExpeditionBatchAttributes {
   id: string;
   number: string;
@@ -8,6 +11,32 @@ export interface ExpeditionBatchAttributes {
   total_volumes: number;
   createdAt?: Date;
   updatedAt?: Date;
+  batchInvoices?: any;
+
 }
+
+export interface InvoiceFull extends InvoiceAttributes {
+  items: InvoiceItemsAttributes[]
+}
+
+export interface ExpeditionBatchInvoiceFull extends ExpeditionBatchInvoiceAttributes {
+  invoice: InvoiceFull
+}
+
+export interface ExpeditionBatchFull {
+  id: string;
+  number: string;
+  status: 'OPEN' | 'PENDING' | 'FINISHED';
+  integrations_id?: string;
+  id_system?: string;
+  unit_business_id: string;
+  total_volumes: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+  batchInvoices?: ExpeditionBatchInvoiceFull[];
+
+}
+
+
 
 export interface ExpeditionBatchCreationAttributes extends Omit<ExpeditionBatchAttributes, 'id' | 'createdAt' | 'updatedAt'> {}
