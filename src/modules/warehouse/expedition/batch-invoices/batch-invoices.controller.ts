923c447e2182f421209df3efeaba5b2c67df5c1b
Gabz047 <gabe1612mail@gmail.com>
2026-04-30 18:07:14 -0300

    FEAT: Created batch invoice remove method

--- src/modules/warehouse/expedition/batch-invoices/batch-invoices.controller.ts
+++ src/modules/warehouse/expedition/batch-invoices/batch-invoices.controller.ts
@@ -1,11 +1,41 @@
+import { authenticate } from '../../../../middlewares/auth-token';
 import BaseController from '../../../../shared/utils/base-models/base-controller';
 import ExpeditionBatchInvoice from './batch-invoices.model';
 import ExpeditionBatchInvoiceService from './batch-invoices.service';
+import { Request, Response } from 'express';
 
 export class ExpeditionBatchInvoiceController extends BaseController<ExpeditionBatchInvoice, typeof ExpeditionBatchInvoiceService> {
   constructor() {
     super(ExpeditionBatchInvoiceService);
+
+    this.router.delete("/remove/:id", this.removeBatchInvoice)
   }
+
+    protected middlewaresFor() {
+        return {
+          index: [authenticate],
+          create: [authenticate],
+          update: [
+            authenticate,
+          ],
+          show: [authenticate],
+          destroy: [authenticate],
+          removeBatchInvoice: [authenticate]
+        };
+      }
+
+      removeBatchInvoice = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const {id} = req.params
+
+      await this.service.removeBatchInvoice(id as string)
+
+    return res.status(201).json({ message: "Nota removida com sucesso!" });
+    } catch (error: any) {
+      return res.status(400).json({error: error.message})
+    }
+  }
+
 }
 
 export default new ExpeditionBatchInvoiceController();
--- src/modules/warehouse/expedition/batch-invoices/batch-invoices.service.ts
+++ src/modules/warehouse/expedition/batch-invoices/batch-invoices.service.ts
@@ -1,12 +1,141 @@
-import BaseService from '../../../../shared/utils/base-models/base-service';
-import ExpeditionBatchInvoice from './batch-invoices.model';
-import expeditionBatchInvoiceRepository, { ExpeditionBatchInvoiceRepository } from './batch-invoices.repository';
+import { Op } from "sequelize";
+import sequelize from "../../../../config/sequelize";
+import BaseService from "../../../../shared/utils/base-models/base-service";
+import invoiceItemsService from "../../entrance/invoice-items/invoice-items.service";
+import invoiceService from "../../entrance/invoice/invoice.service";
+import ExpeditionBatchItems from "../batch-items/batch-items.model";
+import ExpeditionBatch from "../batch/batch.model";
+import scanLogsService from "../scan-logs/scan-logs.service";
+import ExpeditionBatchInvoice from "./batch-invoices.model";
+import expeditionBatchInvoiceRepository, {
+  ExpeditionBatchInvoiceRepository,
+} from "./batch-invoices.repository";
 
-export class ExpeditionBatchInvoiceService extends BaseService<ExpeditionBatchInvoice, ExpeditionBatchInvoiceRepository> {
+export class ExpeditionBatchInvoiceService extends BaseService<
+  ExpeditionBatchInvoice,
+  ExpeditionBatchInvoiceRepository
+> {
   constructor() {
     super(expeditionBatchInvoiceRepository);
-      
+  }
+
+  async removeBatchInvoice(id: string): Promise<void> {
+    return await sequelize.transaction(async (t) => {
+      const batchInvoice = (await this.findById(id, {
+        transaction: t,
+      })) as ExpeditionBatchInvoice;
+
+      if (!batchInvoice) {
+        throw new Error(`Nota fiscal não encontrada`);
+      }
+
+      //Procura
+
+      const scanLogs = await scanLogsService.findAll({
+        where: {
+          expedition_batch_id: batchInvoice.expedition_batch_id,
+          expedition_batch_invoices_id: batchInvoice.id,
+        },
+        transaction: t,
+      });
+
+      const invoiceFound = await invoiceService.findById(
+        batchInvoice.invoice_id,
+        { transaction: t },
+      );
+
+      if (!invoiceFound) {
+        throw new Error(`Nota fiscal não existe no sistema`);
+      }
+
+      const invoiceItemsFound = await invoiceItemsService.findAll({
+        where: {
+          invoice_id: invoiceFound.id,
+        },
+        transaction: t,
+      });
+
+      if (!invoiceItemsFound.length) {
+        throw new Error(`Nota fiscal não possui itens`);
+      }
+
+      for (const item of invoiceItemsFound) {
+        await ExpeditionBatchItems.decrement("quantity", {
+          by: item.quantity_expected,
+          where: {
+            expedition_batch_id: batchInvoice.expedition_batch_id,
+            product_id: item.product_id,
+          },
+          transaction: t,
+        });
+
+        await ExpeditionBatchItems.decrement("quantity_scanned", {
+          by: item.quantity_received,
+          where: {
+            expedition_batch_id: batchInvoice.expedition_batch_id,
+            product_id: item.product_id,
+          },
+          transaction: t,
+        });
+
+        await ExpeditionBatch.decrement("total_volumes", {
+          by: item.quantity_expected,
+          where: { id: batchInvoice.expedition_batch_id },
+          transaction: t,
+        });
+
+        await ExpeditionBatch.decrement("total_volumes_received", {
+          by: item.quantity_received,
+          where: { id: batchInvoice.expedition_batch_id },
+          transaction: t,
+        });
+      }
+
+      await ExpeditionBatchItems.destroy({
+        where: {
+          expedition_batch_id: batchInvoice.expedition_batch_id,
+          quantity: { [Op.lte]: 0 },
+        },
+        transaction: t,
+      });
+
+      // Atualiza invoice items, zera quantidades e passa todas para pendente
+      const invoiceItemsId = invoiceItemsFound.map((s) => s.id);
+      await invoiceItemsService.bulkUpdate(
+        {
+          quantity_received: 0,
+          status: "PENDING",
+        },
+        {
+          where: {
+            id: invoiceItemsId,
+          },
+          transaction: t,
+        },
+      );
+
+      await invoiceService.update(
+        batchInvoice.invoice_id,
+        { status: "PENDING", batch_generated: false },
+        { transaction: t },
+      );
+
+      await scanLogsService.bulkDelete({
+        where: {
+          expedition_batch_id: batchInvoice.expedition_batch_id,
+          expedition_batch_invoices_id: batchInvoice.id,
+        },
+        transaction: t,
+      });
+
+      await ExpeditionBatchInvoice.destroy({
+        where: {
+          id: batchInvoice.id,
+        },
+        transaction: t,
+      });
+
+    });
   }
 }
-
 export default new ExpeditionBatchInvoiceService();
