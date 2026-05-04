3015eafb609c573410d8abe04a39b8a267c1c038
Gabz047 <gabe1612mail@gmail.com>
2026-05-04 14:24:38 -0300

    FEAT: Created function to create batch item

--- src/modules/warehouse/entrance/invoice-items/invoice-items.controller.ts
+++ src/modules/warehouse/entrance/invoice-items/invoice-items.controller.ts
@@ -2,10 +2,13 @@ import { authenticate } from '../../../../middlewares/auth-token';
 import BaseController from '../../../../shared/utils/base-models/base-controller';
 import InvoiceItems from './invoice-items.model';
 import InvoiceItemsService from './invoice-items.service';
+import { Request, Response } from "express";
 
 export class InvoiceItemsController extends BaseController<InvoiceItems, typeof InvoiceItemsService> {
   constructor() {
     super(InvoiceItemsService);
+
+    this.router.post("/add/item", this.createInvoiceItem)
   }
 
    protected middlewaresFor() {
@@ -18,8 +21,22 @@ export class InvoiceItemsController extends BaseController<InvoiceItems, typeof 
           show: [authenticate],
           destroy: [authenticate],
           login: [authenticate],
+          createInvoiceItem: [authenticate]
         };
       }
+
+      createInvoiceItem = async (req: Request, res: Response): Promise<Response> => {
+    try {
+       const { invoiceItem, newEan, unMappedProductId } = req.body;
+
+
+      const response = await this.service.createInvoiceItem(invoiceItem, newEan, unMappedProductId)
+
+      return res.status(201).json(response);
+    } catch (error: any) {
+      return res.status(400).json({ error: error.message });
+    }
+  };
 }
 
 export default new InvoiceItemsController();
