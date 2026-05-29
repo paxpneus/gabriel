aa11646ba0739fc1193accc338be383600d1427b
Gabz047 <gabe1612mail@gmail.com>
2026-05-29 09:25:37 -0300

    FEAT: Validation added to not allow delete batch invoice from finished or cancelled batches

--- src/modules/warehouse/expedition/batch-invoices/batch-invoices.service.ts
+++ src/modules/warehouse/expedition/batch-invoices/batch-invoices.service.ts
@@ -19,111 +19,131 @@ export class ExpeditionBatchInvoiceService extends BaseService<
     super(expeditionBatchInvoiceRepository);
   }
 
-  async removeBatchInvoice(id: string, externalTransaction?: Transaction): Promise<void> {
-  const run = async (t: Transaction) => {
-    const batchInvoice = (await this.findById(id, { transaction: t })) as ExpeditionBatchInvoice;
+  async removeBatchInvoice(
+    id: string,
+    externalTransaction?: Transaction,
+  ): Promise<void> {
+    const run = async (t: Transaction) => {
+      const batchInvoice = (await this.findById(id, {
+        transaction: t,
+      })) as ExpeditionBatchInvoice;
 
-    if (!batchInvoice) {
-      throw new Error(`Nota fiscal não encontrada`);
-    }
+      if (!batchInvoice) {
+        throw new Error(`Nota fiscal não encontrada`);
+      }
 
-    const scanLogs = await scanLogsService.findAll({
-      where: {
-        expedition_batch_id: batchInvoice.expedition_batch_id,
-        expedition_batch_invoices_id: batchInvoice.id,
-      },
-      transaction: t,
-    });
+      const expeditionBatch = await ExpeditionBatch.findByPk(
+        batchInvoice.expedition_batch_id,
+      );
 
-    const invoiceFound = await invoiceService.findById(batchInvoice.invoice_id, { transaction: t });
+      if (!expeditionBatch) {
+        throw new Error(`Lote não encontrado!`);
+      }
 
-    if (!invoiceFound) {
-      throw new Error(`Nota fiscal não existe no sistema`);
-    }
+      if (["FINISHED", "CANCELLED"].includes(expeditionBatch!.status)) {
+        throw new Error(`Não é possível remover notas fiscais de lotes finalizados ou cancelados.`);
+      }
 
-    const invoiceItemsFound = await invoiceItemsService.findAll({
-      where: { invoice_id: invoiceFound.id },
-      transaction: t,
-    });
-
-    if (!invoiceItemsFound.length) {
-      throw new Error(`Nota fiscal não possui itens`);
-    }
-
-    for (const item of invoiceItemsFound) {
-      await ExpeditionBatchItems.decrement("quantity", {
-        by: item.quantity_expected,
+      const scanLogs = await scanLogsService.findAll({
         where: {
           expedition_batch_id: batchInvoice.expedition_batch_id,
-          product_id: item.product_id,
+          expedition_batch_invoices_id: batchInvoice.id,
         },
         transaction: t,
       });
 
-      await ExpeditionBatchItems.decrement("quantity_scanned", {
-        by: item.quantity_received,
+      const invoiceFound = await invoiceService.findById(
+        batchInvoice.invoice_id,
+        { transaction: t },
+      );
+
+      if (!invoiceFound) {
+        throw new Error(`Nota fiscal não existe no sistema`);
+      }
+
+      const invoiceItemsFound = await invoiceItemsService.findAll({
+        where: { invoice_id: invoiceFound.id },
+        transaction: t,
+      });
+
+      if (!invoiceItemsFound.length) {
+        throw new Error(`Nota fiscal não possui itens`);
+      }
+
+      for (const item of invoiceItemsFound) {
+        await ExpeditionBatchItems.decrement("quantity", {
+          by: item.quantity_expected,
+          where: {
+            expedition_batch_id: batchInvoice.expedition_batch_id,
+            product_id: item.product_id,
+          },
+          transaction: t,
+        });
+
+        await ExpeditionBatchItems.decrement("quantity_scanned", {
+          by: item.quantity_received,
+          where: {
+            expedition_batch_id: batchInvoice.expedition_batch_id,
+            product_id: item.product_id,
+          },
+          transaction: t,
+        });
+
+        await ExpeditionBatch.decrement("total_volumes", {
+          by: item.quantity_expected,
+          where: { id: batchInvoice.expedition_batch_id },
+          transaction: t,
+        });
+
+        await ExpeditionBatch.decrement("total_volumes_received", {
+          by: item.quantity_received,
+          where: { id: batchInvoice.expedition_batch_id },
+          transaction: t,
+        });
+      }
+
+      await ExpeditionBatchItems.destroy({
         where: {
           expedition_batch_id: batchInvoice.expedition_batch_id,
-          product_id: item.product_id,
+          quantity: { [Op.lte]: 0 },
         },
         transaction: t,
       });
 
-      await ExpeditionBatch.decrement("total_volumes", {
-        by: item.quantity_expected,
-        where: { id: batchInvoice.expedition_batch_id },
+      const invoiceItemsId = invoiceItemsFound.map((s) => s.id);
+      await invoiceItemsService.bulkUpdate(
+        { quantity_received: 0, status: "PENDING" },
+        {
+          where: { id: invoiceItemsId },
+          transaction: t,
+        },
+      );
+
+      await invoiceService.update(
+        batchInvoice.invoice_id,
+        { status: "PENDING", batch_generated: false },
+        { transaction: t },
+      );
+
+      await scanLogsService.bulkDelete({
+        where: {
+          expedition_batch_id: batchInvoice.expedition_batch_id,
+          expedition_batch_invoices_id: batchInvoice.id,
+        },
         transaction: t,
       });
 
-      await ExpeditionBatch.decrement("total_volumes_received", {
-        by: item.quantity_received,
-        where: { id: batchInvoice.expedition_batch_id },
+      await ExpeditionBatchInvoice.destroy({
+        where: { id: batchInvoice.id },
         transaction: t,
       });
+    };
+
+    if (externalTransaction) {
+      return run(externalTransaction);
+    } else {
+      return sequelize.transaction(run);
     }
-
-    await ExpeditionBatchItems.destroy({
-      where: {
-        expedition_batch_id: batchInvoice.expedition_batch_id,
-        quantity: { [Op.lte]: 0 },
-      },
-      transaction: t,
-    });
-
-    const invoiceItemsId = invoiceItemsFound.map((s) => s.id);
-    await invoiceItemsService.bulkUpdate(
-      { quantity_received: 0, status: "PENDING" },
-      {
-        where: { id: invoiceItemsId },
-        transaction: t,
-      },
-    );
-
-    await invoiceService.update(
-      batchInvoice.invoice_id,
-      { status: "PENDING", batch_generated: false },
-      { transaction: t },
-    );
-
-    await scanLogsService.bulkDelete({
-      where: {
-        expedition_batch_id: batchInvoice.expedition_batch_id,
-        expedition_batch_invoices_id: batchInvoice.id,
-      },
-      transaction: t,
-    });
-
-    await ExpeditionBatchInvoice.destroy({
-      where: { id: batchInvoice.id },
-      transaction: t,
-    });
-  };
-
-  if (externalTransaction) {
-    return run(externalTransaction);
-  } else {
-    return sequelize.transaction(run);
   }
-}
 }
 export default new ExpeditionBatchInvoiceService();
