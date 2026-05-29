f2d221e45020bf09f8e47798dabbc2a51309f178
Gabz047 <gabe1612mail@gmail.com>
2026-05-29 18:20:46 -0300

    FIX: Type errors on new structure of applications

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,28 +1,11 @@
 // types/express.d.ts
-import { Request } from 'express';
+import { ApplicationRequestAttributes } from "../../modules/integrations/applications/applications.types";
 
 declare global {
   namespace Express {
     interface Request {
       user?: { id: string; role: string };
-      application?: {
-        id: string;
-        name: string;
-        role_id: string;
-        allowed_routes: string[];
-        rate_limit_max_requests: number;
-        rate_limit_window_seconds: number;
-        token_version: number;
-        is_active: boolean;
-        role?: {
-          id: string;
-          name: string;
-          permissions: {
-            entity: string;
-            permissions: string[];
-          }[];
-        };
-      };
+      application?: ApplicationRequestAttributes;
     }
   }
 }
