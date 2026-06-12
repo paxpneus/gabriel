ea308f05af4e00a1b332b454d2753748da437039
Gabz047 <gabe1612mail@gmail.com>
2026-06-12 00:52:57 -0300

    FIX: Tecinco get incoming invoice

--- src/queues/test-workers.ts
+++ src/queues/test-workers.ts
@@ -23,7 +23,7 @@ export async function startBlingWorkers() {
     workless: false,
   });
 
-  // const tcarApiFetchQueue = new TCarUpsertQueue({ workless: false });
+  const tcarApiFetchQueue = new TCarUpsertQueue({ workless: false });
 
   const dailySalesReportQueue = new SalesReportQueue({ workless: false });
   const autoBackUpQueue = new AutoBackupQueue({ workless: false });
