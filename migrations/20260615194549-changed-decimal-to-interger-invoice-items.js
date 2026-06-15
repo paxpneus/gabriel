9fcd52ac5c0d84191cb6bad2f206c603b55b8f87
Gabz047 <gabe1612mail@gmail.com>
2026-06-15 17:24:28 -0300

    FEAT: Invoice brand

---
+++ migrations/20260615194549-changed-decimal-to-interger-invoice-items.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('invoice_items', 'quantity_expected', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    });
+
+    await queryInterface.changeColumn('invoice_items', 'quantity_received', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('invoice_items', 'quantity_expected', {
+      type: Sequelize.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 0,
+    });
+
+    await queryInterface.changeColumn('invoice_items', 'quantity_received', {
+      type: Sequelize.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 0,
+    });
+  }
+};
