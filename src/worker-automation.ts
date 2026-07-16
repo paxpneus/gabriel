b15d9aee41af411f68969797cabc2c28e817c549
Gabz047 <gabe1612mail@gmail.com>
2026-07-16 15:53:16 -0300

    FIX: start function to worker automation

--- src/worker-automation.ts
+++ src/worker-automation.ts
@@ -1,3 +1,13 @@
+import "dotenv/config";
+import sequelize from "./config/sequelize";
+import { setupAssociations } from "./config/sequelize-associations";
 import { startAutomationWorkers } from "./queues";
 
-startAutomationWorkers();
+async function start(): Promise<void> {
+  await sequelize.authenticate();
+  console.log("------------------- DB: Banco Conectado! -------------------");
+  setupAssociations();
+  startAutomationWorkers();
+}
+
+start();
