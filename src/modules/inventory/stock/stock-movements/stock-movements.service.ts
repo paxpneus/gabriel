960f0fdacd7024d270e09a59779d82ed68692836
Gabz047 <gabe1612mail@gmail.com>
2026-08-03 10:06:42 -0300

    FIX: Fixed manual average cost

--- src/modules/inventory/stock/stock-movements/stock-movements.service.ts
+++ src/modules/inventory/stock/stock-movements/stock-movements.service.ts
@@ -717,10 +717,12 @@ export class StockMovementService extends BaseService<
       );
     }
 
+    if (manualAverageCostValue) {
     await movement.update(
       { manual_average_cost_value: manualAverageCostValue },
       { transaction },
     );
+    }
 
     return this.upsertProductStockMovements(
       productId,
