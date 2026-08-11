b141fa7cf74b1038d6c178f2869c98340ab3607d
Gabz047 <gabe1612mail@gmail.com>
2026-08-11 18:47:00 -0300

    FEAT: Added xml encryption key to setup of jest

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -3,6 +3,9 @@
 
 process.env.JWT_SECRET = "test-secret";
 process.env.NODE_ENV = "test";
+process.env.XML_ENCRYPTION_KEY =
+  process.env.XML_ENCRYPTION_KEY ||
+  "0116d9c244b5b7bac89ba7ecb0c7fd31255b233a6d8950324e57e0d44b53b810";  // 32 bytes hex, valor de teste
 jest.mock("uuid", () => ({
   v4: () => "mock-uuid",
 }));
