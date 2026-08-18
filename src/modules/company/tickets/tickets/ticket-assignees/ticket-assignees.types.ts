93b7beb5a873d540bb459d27da931564b7a05ad3
Gabz047 <gabe1612mail@gmail.com>
2026-08-18 09:33:18 -0300

    FEAT: Created types with inclusion to entities

--- src/modules/company/tickets/tickets/ticket-assignees/ticket-assignees.types.ts
+++ src/modules/company/tickets/tickets/ticket-assignees/ticket-assignees.types.ts
@@ -1,2 +1,13 @@
-export interface TicketAssigneeAttributes { ticket_id: string; user_id: string; assigned_at?: Date; }
+import { User } from "../../../../warehouse";
+import TicketAssignee from "./ticket-assignees.model";
+
+export interface TicketAssigneeAttributes {
+  ticket_id: string;
+  user_id: string;
+  assigned_at?: Date;
+}
 export interface TicketAssigneeCreationAttributes extends TicketAssigneeAttributes {}
+
+export interface FullTicketAssignee extends TicketAssignee {
+    user: User
+}
--- src/modules/company/tickets/tickets/ticket/tickets.types.ts
+++ src/modules/company/tickets/tickets/ticket/tickets.types.ts
@@ -1,3 +1,13 @@
+import Area from "../../../../global/areas/areas.model";
+import { User } from "../../../../warehouse";
+import Priority from "../config/priorities/priorities.model";
+import TicketStatus from "../config/ticket-statuses/ticket-statuses.model";
+import TicketAssignee from "../ticket-assignees/ticket-assignees.model";
+import { FullTicketAssignee } from "../ticket-assignees/ticket-assignees.types";
+import TicketCategoryOption from "../ticket-category-options/ticket-category-options.model";
+import { FullTicketCategoryOption } from "../ticket-category-options/ticket-category-options.types";
+import Ticket from "./tickets.model";
+
 export interface TicketAttributes {
   id: string;
   title: string;
@@ -16,3 +26,12 @@ export interface TicketCreationAttributes extends Omit<
   TicketAttributes,
   "id" | "createdAt" | "updatedAt"
 > {}
+
+export interface FullTicket extends Ticket {
+  requester: User,
+  area: Area,
+  status: TicketStatus,
+  assignees: FullTicketAssignee,
+  categoryOptions: FullTicketCategoryOption[]
+  priority: Priority
+}
