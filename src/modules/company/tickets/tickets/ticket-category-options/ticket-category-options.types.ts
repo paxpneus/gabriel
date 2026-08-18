f4fa3676214d38434e5d8a75937f300b9ffa263e
Gabz047 <gabe1612mail@gmail.com>
2026-08-18 09:33:18 -0300

    FEAT: Created types with inclusion to entities

--- src/modules/company/tickets/tickets/ticket-category-options/ticket-category-options.types.ts
+++ src/modules/company/tickets/tickets/ticket-category-options/ticket-category-options.types.ts
@@ -1,2 +1,12 @@
-export interface TicketCategoryOptionAttributes { ticket_id: string; category_option_id: number; }
+import CategoryOption from "../config/categories/category_options/category-options.model";
+import TicketCategoryOption from "./ticket-category-options.model";
+
+export interface TicketCategoryOptionAttributes {
+  ticket_id: string;
+  category_option_id: number;
+}
 export interface TicketCategoryOptionCreationAttributes extends TicketCategoryOptionAttributes {}
+
+export interface FullTicketCategoryOption extends TicketCategoryOption {
+    categoryOption: CategoryOption
+}
